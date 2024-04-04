6813967e83e0dc228546337e2043d5215dd6e072
Anderson Suassuna <andersonssgf7@gmail.com>
2024-04-04 13:54:24 -0300

    terminei as listas de while e for

--- Listas/Lista2For.py
+++ Listas/Lista2For.py
@@ -55,3 +55,154 @@ def q4():
         else:
             print(f"Aluno {n+1} reprovado!")
         n=n+1
+
+def q5():
+    l=[]
+    n=int(input("Quantos números? "))
+    for i in range(n):
+        cont=1
+        x=float(input(f"Número {cont}: "))
+        l.append(x)
+        cont=cont+1
+    soma=sum(l)
+    print(f"A média foi de {round(soma/n,2)}.")
+
+def q6():
+    for i in range(5):
+        a=int(input("A: "))
+        b=int(input("B: "))
+        c=a
+        a=b
+        b=c
+        if a<0:
+            print("a é negativo!")
+        elif a==0:
+            print("a é zero!")
+        else:
+            print("a é positivo!")
+        if b<0:
+            print("b é negativo!")
+        elif b==0:
+            print("b é zero!")
+        else:
+            print("b é positivo!")
+        print(a,b)
+
+def q7():
+    import itertools as it
+    for i in it.count():
+        anos=int(input("Anos: "))
+        cpd=int(input("Cigarros por dia: "))
+        pcc=float(input("Preço de carteira com 20 cigarros: "))
+        cpa=(anos*365)*cpd
+        print("Você gastou R${} com cigarros".format((cpa*pcc)/20))
+        sn=input("Deseja parar? (s): ")
+        if sn=="s":
+            break
+    print("Obrigado por usar nossa calculadora!")
+
+def q8():
+    import itertools as it
+    for i in it.count():
+        n=input("Nome: ")
+        i=int(input("Idade: "))
+        if i>=50:
+            p="está perto de se aposentar."
+        else:
+            p="não está perto de se aposentar."
+        print("Seu nome é {} e você tem {} anos. Além disso, você {}".format(n,i,p))
+        sn=input("Deseja parar? (s): ")
+        if sn=="s":
+            break
+    print("Obrigado!")
+
+def q9():
+    for i in range(30):
+        anos=int(input("Quantos anos você tem? "))
+        aem=anos*12
+        aed=anos*365
+        aeh=aed*24
+        aemin=aeh*60
+        q=input("Gostaria de saber quantos meses (1), dias (2), horas (3) ou minutos (4)? ")
+        if q=="1":
+            print("Você viveu {} meses.".format(aem))
+        elif q=="2":
+            print("Você viveu {} dias.".format(aed))
+        elif q=="3":
+            print("Você viveu {} horas.".format(aeh))
+        elif q=="4":
+            print("Você viveu {} minutos.".format(aemin))
+        else:
+            print("Insira um número válido!")
+
+def q10():
+    cc=float(input("Consumo do carro em litros/km: "))
+    d= float(input("Distância em km: "))
+    if cc<0 or d<0:
+        print("Não são permitidos valores negativos!")
+        exit()
+    print("Litros que serão gastos: {}".format(cc*d))
+
+def q11():
+    import itertools as it
+    for i in it.count():
+        g=float(input("Preço da gasolina: "))
+        a=float(input("Preço do álcool: "))
+        if g<0 or a<0:
+            print("Não são permitidos valores negativos!")
+            exit()
+        if g<a:
+            print("É mais vantajoso abastecer com gasolina!")
+        elif g==a:
+            print("Os dois têm o mesmo preço!")
+        else:
+            print("É mais vantajoso abastecer com álcool!")
+        sn=input("Deseja calcular denovo? (s): ")
+        if sn !="s":
+            break
+    print("Obrigado por usar nossa calculadora!")
+
+def q12():
+    for i in range(2,1000,2):
+        print(i)
+
+def q13():
+    for i in range(1001,2000):
+        print(i)
+
+def q14():
+    n=int(input("Tabuada do: "))
+    for i in range(1,11):
+        print(f"{n} x {i} = {n*i}")
+
+def q15():
+    useroriginal="AndersonSuassuna"
+    senhaoriginal="suassuna123"
+    if useroriginal==senhaoriginal:
+        print("Seus dados originais não podem ser iguais! Corrija-os!")
+        exit()
+
+    user=input("Nome de usuário: ")
+    senha=input("Senha: ")
+    if user==senha:
+        print("O usuário e a senha não podem ser iguais!")
+        exit()
+    if user==useroriginal and senha==senhaoriginal:
+        print("Você fez login com sucesso!")
+    else:
+        print("Seu usuário ou senha estão incorretos!")
+
+def q16():
+    import itertools as it
+    pares=[]
+    impares=[]
+    for i in it.count():
+        n=int(input("Informe o número: "))
+        if n%2==0:
+            pares.append(n)
+        else:
+            impares.append(n)
+        sn=input("Deseja informar outro número? (s): ")
+        if sn!="s":
+            break
+    print(f"Foram {len(pares)} números pares e {len(impares)} números ímpares")
--- Listas/Lista2While.py
+++ Listas/Lista2While.py
@@ -28,7 +28,7 @@ def q2():
         print(f"Área: {l*l}")
         areas.append(l)
         n=n+1
-    print(f"O maior quadrado foi o de área {l*l} (lado {l})")
+    print(f"O maior quadrado foi o de área {max(areas)})")
 
 def q3():
     n=0
@@ -58,3 +58,164 @@ def q4():
         else:
             print(f"Aluno {n+1} reprovado!")
         n=n+1
+
+def q5():
+    sn="s"
+    numeros=[]
+    while sn=="s":
+        numero=float(input("Número: "))
+        numeros.append(numero)
+        sn=input("Deseja escrever outro número? (s): ")
+        if sn!="s":
+            break
+    print(f"Média: {sum(numeros)/len(numeros)}")
+
+def q6():
+    n=1
+    while n<=5:
+        a=int(input("A: "))
+        b=int(input("B: "))
+        c=a
+        a=b
+        b=c
+        if a<0:
+            print("a é negativo!")
+        elif a==0:
+            print("a é zero!")
+        else:
+            print("a é positivo!")
+        if b<0:
+            print("b é negativo!")
+        elif b==0:
+            print("b é zero!")
+        else:
+            print("b é positivo!")
+        print(a,b)
+        n=n+1
+
+def q7():
+    sn="s"
+    while sn=="s":
+        anos=int(input("Anos: "))
+        cpd=int(input("Cigarros por dia: "))
+        pcc=float(input("Preço de carteira com 20 cigarros: "))
+        cpa=(anos*365)*cpd
+        print("Você gastou R${} com cigarros".format((cpa*pcc)/20))
+        sn=input("Deseja calcular denovo? (s): ")
+        if sn!="s":
+            break
+    print("Obrigado por usar nossa calculadora!")
+
+def q8():
+    sn="s"
+    while sn=="s":
+        n=input("Nome: ")
+        i=int(input("Idade: "))
+        if i>=50:
+            p="está perto de se aposentar."
+        else:
+            p="não está perto de se aposentar."
+        print("Seu nome é {} e você tem {} anos. Além disso, você {}".format(n,i,p))
+        sn=input("Deseja calcular denovo? (s): ")
+        if sn!="s":
+            break
+    print("Obrigado!")
+
+def q9():
+    n=1
+    while n<=30:
+        anos=int(input("Quantos anos você tem? "))
+        aem=anos*12
+        aed=anos*365
+        aeh=aed*24
+        aemin=aeh*60
+        q=input("Gostaria de saber quantos meses (1), dias (2), horas (3) ou minutos (4)? ")
+        if q=="1":
+            print("Você viveu {} meses.".format(aem))
+        elif q=="2":
+            print("Você viveu {} dias.".format(aed))
+        elif q=="3":
+            print("Você viveu {} horas.".format(aeh))
+        elif q=="4":
+            print("Você viveu {} minutos.".format(aemin))
+        else:
+            print("Insira um número válido!")
+        n=n+1
+    print("Obrigado!")
+
+def q10():
+    cc=float(input("Consumo do carro em litros/km: "))
+    d= float(input("Distância em km: "))
+    if cc<0 or d<0:
+        print("Não são permitidos valores negativos!")
+        exit()
+    print("Litros que serão gastos: {}".format(cc*d))
+
+def q11():
+    sn="s"
+    while sn=="s":
+        g=float(input("Preço da gasolina: "))
+        a=float(input("Preço do álcool: "))
+        if g<0 or a<0:
+            print("Não são permitidos valores negativos!")
+            exit()
+        if g<a:
+            print("É mais vantajoso abastecer com gasolina!")
+        elif g==a:
+            print("Os dois têm o mesmo preço!")
+        else:
+            print("É mais vantajoso abastecer com álcool!")
+        sn=input("Deseja calcular denovo? (s): ")
+        if sn !="s":
+            break
+    print("Obrigado por usar nossa calculadora!")
+
+def q12():
+    n=2
+    while n<=1000:
+        print(n)
+        n=n+2
+
+def q13():
+    n=1002
+    while n<2000:
+        print(n)
+        n+=1
+
+def q14():
+    n=1
+    t=int(input("Tabuada do número: "))
+    while n<=10:
+        print(f"{t} x {n} = {t*n}")
+        n+=1
+
+def q15():
+    useroriginal="AndersonSuassuna"
+    senhaoriginal="suassuna123"
+    while useroriginal==senhaoriginal:
+        print("Seus dados originais não podem ser iguais! Corrija-os!")
+        continue
+    user=input("Nome de usuário: ")
+    senha=input("Senha: ")
+    while user==senha:
+        print("O usuário e a senha não podem ser iguais!")
+        continue
+    if user==useroriginal and senha==senhaoriginal:
+        print("Você fez login com sucesso!")
+    else:
+        print("Seu usuário ou senha estão incorretos!")
+
+def q16():
+    sn="s"
+    pares=[]
+    impares=[]
+    while sn=="s":
+        n=int(input("Informe o número: "))
+        if n%2==0:
+            pares.append(n)
+        else:
+            impares.append(n)
+        sn=input("Deseja informar outro número? (s): ")
+        if sn!="s":
+            break
+    print(f"Foram {len(pares)} números pares e {len(impares)} números ímpares.")

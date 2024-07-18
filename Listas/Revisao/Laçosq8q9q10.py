fb17a9292a128ec5c7ba02dd66683d6426184b87
Anderson Suassuna <andersonssgf7@gmail.com>
2024-07-18 13:51:58 -0300

    Update Laçosq8q9q10.py

--- Listas/Revisao/Laçosq8q9q10.py
+++ Listas/Revisao/Laçosq8q9q10.py
@@ -1,28 +1,27 @@
 def q8():
     n=int(input("Número: "))
-    divisores=[]
+    divisores=0
     divisor=0
     for i in range(n): # igual a q6, mas impedindo a divsão n/n, além de guardar o valor dos divisores
         divisor=divisor+1
         if n==divisor:
             break
         if n%divisor==0:
-            divisores.append(divisor)
-    if n==sum(divisores):
+            divisores=divisores+divisor
+    if n==divisores:
         print(f"O número {n} é perfeito!")
     else:
         print(f"O número {n} não é perfeito!")
     print(divisores)
-q8()
 
 def q9():
     n=input("Número: ")
-    algarismos=[]
+    algarismos=0
     casa=0
     for i in range(len(n)):
-        algarismos.append(int(n[casa]))
+        algarismos=algarismos+int(n[casa])
         casa=casa+1
-    print(f"A soma dos dígitos de {n} é {sum(algarismos)}!")
+    print(f"A soma dos dígitos de {n} é {algarismos}!")
 q9()
 
 def q10():
@@ -31,4 +30,3 @@ def q10():
         print(f"{n} é um quadrado perfeito!")
     else:
         print(f"{n} não é um quadrado perfeito!")
-q10()

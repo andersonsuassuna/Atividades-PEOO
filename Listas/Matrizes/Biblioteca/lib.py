3667ee7437b8cdba77fdcb78a7d03e9763db4029
Anderson Suassuna <andersonssgf7@gmail.com>
2024-09-03 00:25:48 -0300

    começando projeto de demetrios sobre biblioteca e matrizes

--- Listas/Matrizes/Biblioteca/lib.py
+++ Listas/Matrizes/Biblioteca/lib.py
@@ -1,3 +1,21 @@
-def mostrar(m):
-    for i in range(len(m)):
-        print(m[i])
+from tkinter import *
+biblioteca=[
+    ["O Pequeno Príncipe", "Antoine de Saint-Exupéry", 1943],
+    ["Romeu e Julieta", "William Shakespeare", 1597]
+]
+
+def mostrar(texto,j,r,c): # Mostrar texto em determinado grid
+    label=Label(j,text=texto)
+    label.grid(row=r,column=c)
+
+def mostrarbiblioteca(): # Mostra a biblioteca inteira em uma nova janela
+    novajanela=Tk()
+    novajanela.title("Biblioteca")
+    for i in range(len(biblioteca)):
+        label=Label(novajanela,text=biblioteca[i])
+        label.grid(row=i, column=0)
+    novajanela.mainloop()
+
+def adicionar():
+    a=[]
+    
--- Listas/Matrizes/Biblioteca/main.py
+++ Listas/Matrizes/Biblioteca/main.py
@@ -1,7 +1,18 @@
-import lib
-estoque=[
-    [1,2,3],
-    [4,5,6],
-    [7,8,9],
-]
-lib.mostrar(estoque)
+from lib import *
+from tkinter import *
+janela=Tk()
+janela.title("Biblioteca")
+
+# Botão de mostrar a biblioteca
+showimg=PhotoImage(file=r"C:\Users\ander\OneDrive\Documentos\MeusProjetos\Atividades-PEOO\Listas\Matrizes\Biblioteca\141947.png")
+showimgmenor=showimg.subsample(4,4) # diminui tamanho da imagem
+mostrarbiblioteca=Button(janela,image=showimgmenor,command= mostrarbiblioteca)
+mostrarbiblioteca.grid(row=0,column=1)
+
+# Botão de adicionar um livro à biblioteca
+addimg=PhotoImage(file=r"C:\Users\ander\OneDrive\Documentos\MeusProjetos\Atividades-PEOO\Listas\Matrizes\Biblioteca\livro-com-botao-adicionar.png")
+addimgmenor=addimg.subsample(4,4) # diminui tamanho da imagem
+addlivro=Button(janela, image=addimgmenor, command=lambda: adicionar(biblioteca,janela))
+addlivro.grid(row=0, column=0)
+
+janela.mainloop() # Mainloop

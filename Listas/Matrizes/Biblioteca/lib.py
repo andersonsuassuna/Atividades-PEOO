cdf5fa2ecc4746017899f48facd6dfe2c0bc276a
Anderson Suassuna <andersonssgf7@gmail.com>
2024-09-03 15:04:35 -0300

    botao de adicionar à biblioteca

--- Listas/Matrizes/Biblioteca/lib.py
+++ Listas/Matrizes/Biblioteca/lib.py
@@ -11,11 +11,44 @@ def mostrar(texto,j,r,c): # Mostrar texto em determinado grid
 def mostrarbiblioteca(): # Mostra a biblioteca inteira em uma nova janela
     novajanela=Tk()
     novajanela.title("Biblioteca")
-    for i in range(len(biblioteca)):
-        label=Label(novajanela,text=biblioteca[i])
-        label.grid(row=i, column=0)
+    
+    titulos=Label(novajanela,text="Título")
+    titulos.grid(row=0, column=0)
+    
+    autores=Label(novajanela,text="Autor")
+    autores.grid(row=0, column=1)
+    
+    datas=Label(novajanela,text="Ano de publicação")
+    datas.grid(row=0, column=2)
+    
+    for i in range(len(biblioteca)): # Coloca todos os dados dos livros em linhas e colunas da nova janela
+        for j in range(3):
+            label=Label(novajanela,text=biblioteca[i][j])
+            label.grid(row=i+1, column=j)
     novajanela.mainloop()
 
-def adicionar():
-    a=[]
+def adicionar(): # Adiciona um novo livro com seus dados fornecidos por Entries
+    janelaadd=Tk()
+    
+    titulo=Label(janelaadd,text="Título: ")
+    tituloinput=Entry(janelaadd)
+    tituloinput.grid(row=0,column=1)
+    titulo.grid(row=0,column=0)
+    
+    autor=Label(janelaadd,text="Autor: ")
+    autorinput=Entry(janelaadd)
+    autorinput.grid(row=1,column=1)
+    autor.grid(row=1,column=0)
+    
+    data=Label(janelaadd,text="Ano de publicação: ")
+    datainput=Entry(janelaadd)
+    datainput.grid(row=2,column=1)
+    data.grid(row=2,column=0)
+    
+    # Esse botão executa dois comandos: um para adicionar o livro na matriz e outro para mostrar na tela que o livro foi adicionado
+    salvar=Button(janelaadd,text="Adicionar Livro", command=lambda: 
+        [biblioteca.append([tituloinput.get(), autorinput.get(), datainput.get()]),
+        mostrar("Livro adicionado!", janelaadd,3,0)])
+    salvar.grid(row=3,column=1)
+    janelaadd.mainloop()
     
--- Listas/Matrizes/Biblioteca/main.py
+++ Listas/Matrizes/Biblioteca/main.py
@@ -1,18 +1,19 @@
 from lib import *
 from tkinter import *
+
 janela=Tk()
 janela.title("Biblioteca")
 
 # Botão de mostrar a biblioteca
-showimg=PhotoImage(file=r"C:\Users\ander\OneDrive\Documentos\MeusProjetos\Atividades-PEOO\Listas\Matrizes\Biblioteca\141947.png")
+showimg=PhotoImage(file=r"C:\Users\ander\OneDrive\Documentos\MeusProjetos\Atividades-PEOO\Listas\Matrizes\Biblioteca\img\141947.png")
 showimgmenor=showimg.subsample(4,4) # diminui tamanho da imagem
 mostrarbiblioteca=Button(janela,image=showimgmenor,command= mostrarbiblioteca)
 mostrarbiblioteca.grid(row=0,column=1)
 
 # Botão de adicionar um livro à biblioteca
-addimg=PhotoImage(file=r"C:\Users\ander\OneDrive\Documentos\MeusProjetos\Atividades-PEOO\Listas\Matrizes\Biblioteca\livro-com-botao-adicionar.png")
+addimg=PhotoImage(file=r"C:\Users\ander\OneDrive\Documentos\MeusProjetos\Atividades-PEOO\Listas\Matrizes\Biblioteca\img\livro-com-botao-adicionar.png")
 addimgmenor=addimg.subsample(4,4) # diminui tamanho da imagem
-addlivro=Button(janela, image=addimgmenor, command=lambda: adicionar(biblioteca,janela))
+addlivro=Button(janela, image=addimgmenor, command=adicionar)
 addlivro.grid(row=0, column=0)
 
 janela.mainloop() # Mainloop

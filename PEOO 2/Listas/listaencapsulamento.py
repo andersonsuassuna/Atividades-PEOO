cb24857ff82b4b77a5ea31312da8d3f064f45dd8
Anderson Suassuna <andersonssgf7@gmail.com>
2024-11-21 13:02:43 -0300

    terminei a lista de encapsulamento

--- PEOO 2/Listas/listaencapsulamento.py
+++ PEOO 2/Listas/listaencapsulamento.py
@@ -29,14 +29,45 @@ class BombaCombustivel:
     
     def get_tipoCombustivel(self):
         return self.__tipoCombustivel
+    
+    def get_quantidadeCombustivel(self):
+        return self.__quantidadeCombustivel
 
+# EXTRA
 class Veiculo:
     def __init__(self,litrosnotanque,limite,combustivel):
         self.litrosnotanque=litrosnotanque
         self.limite=limite
         self.combustivel=combustivel
-        
+    
+    def abastecerPorLitro(self,litros):
+        if self.litrosnotanque+litros<=self.limite:
+            self.litrosnotanque+=litros
+        else:
+            print("O tanque não aguenta tanto combustível!")
+    
+    def alterarLimite(self,limite):
+        self.limite=limite
+    
+    def alterarCombustivel(self,combustivel):
+        self.combustivel=combustivel
 
 bomba=BombaCombustivel("Gasolina",5,100)
 bomba.alterarCombustivel("Alcool")
 print(bomba.get_tipoCombustivel())
+veiculo=Veiculo(10,100,"Alcool")
+
+def usarBombaEmVeiculo(Bomba,Veiculo,litros):
+    if Bomba.get_tipoCombustivel()==Veiculo.combustivel:
+        Bomba.abastecerPorLitro(litros)
+        Veiculo.abastecerPorLitro(litros)
+    else:
+        print("Os combustíveis são diferentes!")
+
+print(veiculo.litrosnotanque)
+print(bomba.get_quantidadeCombustivel())
+
+usarBombaEmVeiculo(bomba,veiculo,20)
+
+print(veiculo.litrosnotanque)
+print(bomba.get_quantidadeCombustivel())

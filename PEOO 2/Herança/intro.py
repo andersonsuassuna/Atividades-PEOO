cdbad5c99ae8c3a8e7c740fcf30ada34a6d72089
Anderson Suassuna <andersonssgf7@gmail.com>
2025-01-16 17:34:58 -0300

    Update intro.py

--- PEOO 2/Herança/intro.py
+++ PEOO 2/Herança/intro.py
@@ -16,7 +16,7 @@ class Contact:
         self.email=email
         Contact.all_contacts.append(self)
     
-    def __str__(self):
+    def __str__(self): # configurar print de uma classe
         return(f"{self.name} {self.email}")
 
 class Supplier(Contact):
@@ -36,3 +36,30 @@ c3=Contact("Gustavo","gustavo@gmail.com")
 
 Contact.all_contacts.search("Anderson")
 print(c3)
+
+# Super
+# O Super é usado para herdar os atributos da classe pai. Observe:
+
+class Friend(Contact):
+    def __init__(self, name, email, phone):
+        super().__init__(name, email)
+        self.phone=phone
+
+friend1=Friend("MaisLuan","maisluan@gmail.com", "40028922")
+friend1.phone
+
+class Funcionario:
+    def _init_(self, nome, cpf, salario):
+        self._nome = nome
+        self._cpf = cpf
+        self._salario = salario
+    def get_bonificacao(self):
+        return self.salario * 0.1
+
+class Gerente(Funcionario):
+    def _init_(self, nome, cpf, salario, senha, qtd_gerenciaveis): 
+        super().__init_(nome, cpf, salario)
+        self._senha = senha
+        self._qtd_gerenciaveis = qtd_gerenciaveis
+        def get_bonificacao():
+            return super().get_bonificacao() + 1000
